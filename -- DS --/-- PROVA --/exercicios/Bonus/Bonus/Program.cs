9cd21f7cfade1fa598bdb6e71c825a90e65edfc4
Gabriel Henrique da Silva <ggabriel.hhenrique.ssilva@gmail.com>
2026-03-05 21:35:30 -0300

    Add files via upload

---
+++ -- DS --/-- PROVA --/exercicios/Bonus/Bonus/Program.cs
@@ -0,0 +1,51 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Bonus
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int vAnosEmpresa;
+                string vAvaliação;
+                double vSalario, vBonus;
+
+                Console.Write("Você tem quantos anos de empresa? --> ");
+                vAnosEmpresa = int.Parse(Console.ReadLine());
+
+                Console.Write("Você tem que avaliação? --> ");
+                vAvaliação = Console.ReadLine();
+
+                Console.Write("Quanto você recebe de salário? --> R$");
+                vSalario = double.Parse(Console.ReadLine());
+
+                if (vAnosEmpresa >= 5)
+                {
+                    if (vAvaliação == "a" || vAvaliação == "A")
+                    {
+                        vBonus = vSalario + (vSalario * 0.20);
+                        Console.WriteLine("Seu salário com bônus acrescidos é de: R$" + vBonus);
+                    }
+                    else if (vAvaliação == "b" || vAvaliação == "B")
+                    {
+                        vBonus = vSalario + (vSalario * 0.10);
+                        Console.WriteLine("Seu salário com bônus acrescidos é de: R$" + vBonus);
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Essa avaliação não dá direito a bônus!");
+                    }
+           
+                } 
+                else
+                {
+                    Console.WriteLine("Você precisa de no mínimo 5 anos de empresa para receber bônus");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
---
+++ -- DS --/-- PROVA --/exercicios/desconto/desconto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desconto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vValorCompra, vValorDesconto;
+
+            Console.Write("Insira aqui o valor de sua compra --> R$");
+            vValorCompra = double.Parse(Console.ReadLine());
+
+            if (vValorCompra > 100)
+            {
+                vValorDesconto = vValorCompra - (vValorCompra * 0.10);
+                Console.WriteLine("O valor final de sua compra é de R$" + vValorDesconto);
+            }
+            else
+            {
+                Console.WriteLine("O valor final de sua compra é de R$" + vValorCompra);
+            }
+            Console.ReadKey();
+        }
+    }
+}
